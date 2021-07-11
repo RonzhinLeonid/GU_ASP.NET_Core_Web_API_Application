f3a8d18c1ecb69b4a8e60f6d39f58ac417578291
Leonid <jiehuh87@yandex.ru>
2021-07-12 00:16:42 +0300

    ДЗ версия2

---
+++ GU_ASP.NET_Core_Web_API_Application/Les1_V2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Les1_V2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Work("https://jsonplaceholder.typicode.com", "result1.json", 4, 20).Wait();
+            Console.WriteLine("1");
+            Work("https://jsonplaceholder.typicode.com", "result2.json", 5, 20).Wait();
+            Console.WriteLine("2");
+            Work("https://jsonplaceholder.typicode.com", "result3.json", -1, 20).Wait();
+            Console.WriteLine("3");
+            Task.WaitAll();
+        }
+
+        private static async Task Work(string apiUrl, string pathToFile, int start = 1, int count = 1)
+        {
+            await Task.Run(() => WorkAsync(apiUrl, pathToFile, start, count));
+            Task.WaitAll();
+        }
+
+        //private static async void WorkAsync(string apiUrl, string pathToFile, int start, int count)
+        //{
+        //    var serviceLoadPost = new ServiceLoadPost(apiUrl);
+        //    var posts = await serviceLoadPost.LoadPostsAsync(start, count);
+        //    await SaveToFile.SaveToFileAsync(pathToFile, posts);
+        //}
+
+        private static void WorkAsync(string apiUrl, string pathToFile, int start, int count)
+        {
+            var serviceLoadPost = new ServiceLoadPost(apiUrl);
+            var posts = serviceLoadPost.LoadPostsAsync(start, count);
+            SaveToFile.SaveFile(pathToFile, posts.Result);
+        }
+    }
+}
