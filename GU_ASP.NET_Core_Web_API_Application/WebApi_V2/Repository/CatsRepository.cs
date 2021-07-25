42a4a030fca65bcc4fba32144c2b5572f0f22b73
Leonid <jiehuh87@yandex.ru>
2021-07-25 20:49:30 +0300

    Внесены изменения на основе замечаний из 2го дз

--- GU_ASP.NET_Core_Web_API_Application/WebApi_V2/Repository/CatsRepository.cs
+++ GU_ASP.NET_Core_Web_API_Application/WebApi_V2/Repository/CatsRepository.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApi_V2.DAL;
 
 namespace WebApi_V2.Repository
 {
@@ -13,15 +14,47 @@ namespace WebApi_V2.Repository
 
         public CatsRepository(ApplicationDataContext context) => _context = context;
 
-        public void Add(Cat cat)
+        public async Task Add(Cat cat)
         {
             _context.Set<Cat>().Add(cat);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
         }
 
-        public IList<Cat> Get()
+        public async Task<IList<Cat>> Get()
         {
-            return _context.Set<Cat>().ToList();
+            return await _context.Set<Cat>().ToListAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var catDelete = await _context.Set<Cat>().SingleOrDefaultAsync(t => t.Id == id);
+            if (catDelete is null) return;
+            _context.Set<Cat>().Remove(catDelete);
+            await _context.SaveChangesAsync();
+        }
+
+        
+        public async Task Update(Cat cat)
+        {
+            _context.Set<Cat>().Update(cat);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IList<Cat>> GetFilterName(SearchWithPageRequest searchWithPage)
+        {
+            if (searchWithPage.Page < 1) return new List<Cat>();
+
+            return await _context.Set<Cat>()
+                .Where(p => p.Nickname.ToLower().Contains(searchWithPage.Nickname.ToLower()))
+                .Skip((searchWithPage.Page - 1) * searchWithPage.Size)
+                .Take(searchWithPage.Size)
+                .ToListAsync();
+        }
+        public async Task<IList<Cat>> GetFilterName(SearcRequest search)
+        {
+            return await _context.Set<Cat>()
+                .Where(p => p.Nickname.ToLower().Contains(search.Nickname.ToLower()))
+                .ToListAsync();
         }
     }
 }
